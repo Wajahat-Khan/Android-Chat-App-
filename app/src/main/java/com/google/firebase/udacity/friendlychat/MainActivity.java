0c4e11a26c36bdc2b7767e14f330bbd858965610
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2018-08-12 01:52:20 +0500

    RemoteConfig Enabled

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -37,6 +37,8 @@ import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
 import com.google.android.gms.tasks.Continuation;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -45,13 +47,17 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
     private FirebaseDatabase firebaseDatabase;
@@ -61,12 +67,13 @@ public class MainActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener authStateListener;
     private FirebaseStorage firebaseStorage;
     private StorageReference storageReference;
-    UploadTask fileuploadtask;
-
+    private UploadTask fileuploadtask;
+    private FirebaseRemoteConfig firebaseRemoteConfig;
     private static final String TAG = "MainActivity";
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
+    private static final String FRIENDLY_MSG_LENGTH_KEY="friendly_msg_length";
     public static final int RC_SIGN_IN=1;
     private static final int RC_PHOTO_PICKER =  2;
 
@@ -90,10 +97,11 @@ public class MainActivity extends AppCompatActivity {
         firebaseDatabase = FirebaseDatabase.getInstance();
         mfirebaseAuth = FirebaseAuth.getInstance();
         firebaseStorage=FirebaseStorage.getInstance();
-
+        firebaseRemoteConfig=FirebaseRemoteConfig.getInstance();
 
         databaseReference = firebaseDatabase.getReference().child("messages");
         storageReference=firebaseStorage.getReference().child("chat_photos");
+
 
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
@@ -171,6 +179,12 @@ public class MainActivity extends AppCompatActivity {
         }
         }
     };
+        FirebaseRemoteConfigSettings configSettings=new FirebaseRemoteConfigSettings.Builder().setDeveloperModeEnabled(BuildConfig.DEBUG).build();
+        firebaseRemoteConfig.setConfigSettings(configSettings);
+        Map<String,Object> defaultConfigMap=new HashMap<>();
+        defaultConfigMap.put(FRIENDLY_MSG_LENGTH_KEY,DEFAULT_MSG_LENGTH_LIMIT);
+        firebaseRemoteConfig.setDefaults(defaultConfigMap);
+        fetchConfig();
     }
 
     @Override
@@ -285,5 +299,28 @@ public class MainActivity extends AppCompatActivity {
             databaseReference.removeEventListener(childEventListener);
             childEventListener=null;
         }
+    }
+    public void fetchConfig(){
+        long cacheExpiration=3600;
+        if(firebaseRemoteConfig.getInfo().getConfigSettings().isDeveloperModeEnabled()){
+            cacheExpiration=0;
+        }
+        firebaseRemoteConfig.fetch(cacheExpiration).addOnSuccessListener(new OnSuccessListener<Void>() {
+            @Override
+            public void onSuccess(Void aVoid) {
+                firebaseRemoteConfig.activateFetched();
+                applyRetrievedLengthLimit();
+            }
+
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                applyRetrievedLengthLimit();
+            }
+        });
+    }
+    private void applyRetrievedLengthLimit(){
+        Long friendly_msg_length=firebaseRemoteConfig.getLong(FRIENDLY_MSG_LENGTH_KEY);
+        mMessageEditText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(friendly_msg_length.intValue())});
     }
 }
