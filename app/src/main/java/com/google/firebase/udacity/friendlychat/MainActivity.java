d7103466e49f8de9c6ceb914c2025adbbbe7ed8d
Wajahat Hussain <wkhan.bese15seecs@seecs.edu.pk>
2018-08-11 18:37:52 +0500

    Picture Functionality added. User can select and send the image. Firebase Storage is being used to store the image.

--- app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
+++ app/src/main/java/com/google/firebase/udacity/friendlychat/MainActivity.java
@@ -16,6 +16,7 @@
 package com.google.firebase.udacity.friendlychat;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -34,6 +35,9 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.ChildEventListener;
@@ -41,6 +45,9 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -52,12 +59,16 @@ public class MainActivity extends AppCompatActivity {
     private ChildEventListener childEventListener;
     private FirebaseAuth mfirebaseAuth;
     private FirebaseAuth.AuthStateListener authStateListener;
+    private FirebaseStorage firebaseStorage;
+    private StorageReference storageReference;
+    UploadTask fileuploadtask;
 
     private static final String TAG = "MainActivity";
 
     public static final String ANONYMOUS = "anonymous";
     public static final int DEFAULT_MSG_LENGTH_LIMIT = 1000;
     public static final int RC_SIGN_IN=1;
+    private static final int RC_PHOTO_PICKER =  2;
 
     private ListView mMessageListView;
     private MessageAdapter mMessageAdapter;
@@ -78,8 +89,12 @@ public class MainActivity extends AppCompatActivity {
         // Initialize Firebase components
         firebaseDatabase = FirebaseDatabase.getInstance();
         mfirebaseAuth = FirebaseAuth.getInstance();
+        firebaseStorage=FirebaseStorage.getInstance();
+
+
         databaseReference = firebaseDatabase.getReference().child("messages");
-        // Initialize references to views
+        storageReference=firebaseStorage.getReference().child("chat_photos");
+
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mMessageListView = (ListView) findViewById(R.id.messageListView);
         mPhotoPickerButton = (ImageButton) findViewById(R.id.photoPickerButton);
@@ -98,7 +113,10 @@ public class MainActivity extends AppCompatActivity {
         mPhotoPickerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // TODO: Fire an intent to show an image picker
+                Intent intent= new Intent(Intent.ACTION_GET_CONTENT);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_LOCAL_ONLY,true);
+                startActivityForResult(Intent.createChooser(intent,"Complete action using"), RC_PHOTO_PICKER);
             }
         });
 
@@ -178,15 +196,46 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode==RC_SIGN_IN){
-            if(requestCode==RESULT_OK){
+            if(resultCode==RESULT_OK){
                 Toast.makeText(this, "You are Signed in", Toast.LENGTH_SHORT).show();
             }
-            else{
+            else if(resultCode==RESULT_CANCELED){
                 Toast.makeText(this, "SIgned in Unsuccessful", Toast.LENGTH_SHORT).show();
                 finish();
-            }
+                }
         }
-    }
+            else if(requestCode==RC_PHOTO_PICKER && resultCode==RESULT_OK){
+                Uri selectedImageUri=data.getData();
+                final StorageReference photRef= storageReference.child(selectedImageUri.getLastPathSegment());
+            fileuploadtask = photRef.putFile(selectedImageUri);
+
+            Task<Uri> urlTask = fileuploadtask.continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+                @Override
+                public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+
+                    // Continue with the task to get the download URL
+                    return photRef.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        Uri downloadUri = task.getResult();
+                        FriendlyMessage friendlyMessage=new FriendlyMessage(null, mUsername, downloadUri.toString());
+                        databaseReference.push().setValue(friendlyMessage);
+                         } else {
+                        // Handle failures
+                        // ...
+                    }
+                }
+            });
+                }
+
+        }
+
 
     @Override
     protected void onResume() {
